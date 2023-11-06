e26e9f80757e27b2e5ee2168f4523a701d236e45
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-11-05 19:03:01 -0500

    Update env.go

--- configs/env.go
+++ configs/env.go
@@ -2,13 +2,12 @@ package configs
 
 import (
 	"DENV_Register_Producer/models"
-	"os"
 )
 
 func GetKafkaProperties() (models.KafkaProperties, error) {
 	kfProperties := models.KafkaProperties{
-		Host: os.Getenv("KAFKA_HOST"),
-		Port: os.Getenv("KAFKA_PORT"),
+		Host: "192.168.1.13",
+		Port: "9092",
 	}
 
 	return kfProperties, nil
